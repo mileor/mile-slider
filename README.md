4b553a00dfbb5259a8a2623e6c35a6b0161c097e
mileor <mileorcode@gmail.com>
2020-11-26 20:22:45 +0300

    Init

---
+++ README.md
@@ -0,0 +1,75 @@
+# Mile slider
+
+Simple, customizable, responsive carousel on vanilla JS. 
+[Demo](http://github.com/mileor/mile-slider)
+
+### Install
+
+`npm install mile-slider`
+
+### Usage
+
+1. Add HTML markup:
+```
+<div class="mile-slider" id="slider1">
+  <button class="mile-slider__nav-button mile-slider__nav-button--left">←</button>
+  <button class="mile-slider__nav-button mile-slider__nav-button--right">→</button>
+  <div class="mile-slider__content">
+    <div>
+      <div>
+        <img src="https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="">
+      </div>
+      <div>
+        <img src="https://images.unsplash.com/photo-1459716854666-062eac2da3bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjI5MzI0fQ&auto=format&fit=crop&w=889&q=80" alt="">
+      </div>
+      <div>
+        <img src="https://images.unsplash.com/photo-1437322213019-20ad95053704?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="">
+      </div>
+      <div>
+        <img src="https://images.unsplash.com/photo-1461703048574-9a342517f1bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjF9&auto=format&fit=crop&w=889&q=80" alt="">
+      </div>
+      <div>
+        <img src="https://images.unsplash.com/photo-1441239372925-ac0b51c4c250?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80" alt="">
+      </div>
+      <div>
+        <img src="https://images.unsplash.com/photo-1455487276747-341d543aa498?ixlib=rb-1.2.1&auto=format&fit=crop&w=755&q=80" alt="">
+      </div>
+      <div>
+        <img src="https://images.unsplash.com/photo-1516655855035-d5215bcb5604?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" alt="">
+      </div>
+    </div>
+  </div>
+</div>
+```
+2. Initialize slider in .js file and call mileSlider()
+```
+import { mileSlider } from "./node_modules/mile-slider/src/mile-slider";
+
+mileSlider({
+  container: 'slider1',
+  animationDuration: 600,
+  animationType: 'linear',
+  height: 332,
+  slidesToShow: 3,
+  gap: 24,
+  activeSlideStyles: {
+    backgroundColor: '#6554C0',
+    padding: '16px',
+  }
+});
+```
+### Options
+
+| Option              | Type                                         | Description                 |
+| ------------------- | -------------------------------------------- | --------------------------- |
+| `container`         | String                                       | Slider container ID         |
+| `animationDuration` | Number (positive integer)                    | Time in milliseconds between switching slides |
+| `animationType`     | String (linear, ease, ease-in, ease-out, etc)| [Animation-timing-function](https://developer.mozilla.org/ru/docs/Web/CSS/animation-timing-function) |
+| `height`            | Number (positive integer)                    | Slider height               |
+| `slidesToShow`      | Number (positive integer)                    | Number of slides to scroll  |
+| `gap`               | Number (positive integer)                    | Gap between slides          |
+| `activeSlideStyles` | Object                                       | CSS styles for active slide |
+
+### License
+
+The MIT License.
---
+++ src/mile-slider.js
@@ -0,0 +1,203 @@
+/*
+  Mile-slider
+  Author: Mileor
+*/
+
+function renderMileSlider({
+  container,
+  animationDuration,
+  animationType,
+  height,
+  slidesToShow,
+  gap,
+  activeSlideStyles
+  }) {
+  const sliderContainer      = document.querySelector(`#${container}`);
+  const sliderContentWrapper = document.querySelector(`#${container} .mile-slider__content div`);
+  const sliderContent        = document.querySelectorAll(`#${container} .mile-slider__content div div`);
+  const nextButton           = document.querySelector(`#${container} .mile-slider__nav-button--right`);
+  const prevButton           = document.querySelector(`#${container} .mile-slider__nav-button--left`);
+  const containerWidth       = sliderContainer.getBoundingClientRect().width;
+  const slides               = sliderContent.length;
+
+  const ANIMATION      = `transform ${animationDuration}ms ${animationType}`;
+  const SLIDER_HEIGHT  = height;
+  let SLIDES_TO_SHOW   = slidesToShow > slides ? slides : slidesToShow;
+  const SLIDES_GAP     = gap;
+
+  if (window.screen.width <= 480) {
+    SLIDES_TO_SHOW = 1;
+  } else if (window.screen.width <= 768) {
+    SLIDES_TO_SHOW = 3;
+  }
+
+  const columnWidth       = (containerWidth - (SLIDES_GAP * (SLIDES_TO_SHOW - 1))) / SLIDES_TO_SHOW;
+  let currentNavButton    = null;
+  let currentPosition     = SLIDES_TO_SHOW * (- columnWidth - SLIDES_GAP);
+  let isDisableNavButtons = false;
+
+  sliderContentWrapper.style['grid-template-columns'] = `repeat(auto-fit, ${columnWidth}px)`;
+  sliderContentWrapper.style['grid-gap']              = `${SLIDES_GAP}px`;
+  sliderContentWrapper.style['grid-template-rows']    = `${SLIDER_HEIGHT}px`;
+
+  function setCurrentPosition() {
+    sliderContentWrapper.style.transform = `translateX(${currentPosition}px)`;
+  }
+
+  function setAnimation() {
+    setTimeout(() => {
+      sliderContentWrapper.style.transition = ANIMATION;
+    }, 0)
+  }
+
+  function clearAnimation() {
+    sliderContentWrapper.style.transition = 'none';
+  }
+
+  function setActiveSlide() {
+    if (SLIDES_TO_SHOW % 2 !== 0) {
+      document.querySelectorAll(`#${container} .mile-slider__content div div`).forEach(slide => {
+        slide.classList.remove("mile-slider__active");
+        slide.removeAttribute("style");
+        const containerOffset = sliderContainer.getBoundingClientRect().left;
+        const sliderOffset = slide.getBoundingClientRect().left - containerOffset;
+        const roundedNumberOfSlides = Math.round(((SLIDES_TO_SHOW - 1) / 2));
+        if (Math.round(roundedNumberOfSlides * (columnWidth + SLIDES_GAP)) === Math.round(sliderOffset)) {
+          slide.classList.add("mile-slider__active");
+          const activeSlide = document.querySelector('.mile-slider__active');
+          for (let style in activeSlideStyles) {
+            activeSlide.style[style] = activeSlideStyles[style];
+          }
+        }
+      })
+    }
+  }
+
+  setCurrentPosition();
+  setAnimation();
+
+  for (let i = 0; i <= SLIDES_TO_SHOW - 1; i++) {
+    const clone = sliderContent[i].cloneNode(true);
+    clone.classList.add("mile-slider__clone");
+    sliderContentWrapper.append(clone);
+  }
+
+  for (let i = slides - 1; slides - i <= SLIDES_TO_SHOW; i--) {
+    const clone = sliderContent[i].cloneNode(true);
+    clone.classList.add("mile-slider__clone");
+    sliderContentWrapper.prepend(clone);
+  }
+
+  function nextSlideHandler() {
+    if (currentNavButton === 'next') {
+      isDisableNavButtons = false;
+      const maxSliderRightOffset = (slides + SLIDES_TO_SHOW) * (columnWidth + SLIDES_GAP);
+      if (Math.round(maxSliderRightOffset) === Math.round(Math.abs(currentPosition))) {
+        currentPosition = SLIDES_TO_SHOW * (- columnWidth - SLIDES_GAP);
+        clearAnimation();
+        setCurrentPosition();
+      }
+      setTimeout(() => {
+        setActiveSlide();
+      }, animationDuration / 3)
+      setAnimation();
+      sliderContentWrapper.removeEventListener("transitionend", nextSlideHandler);
+    }
+  }
+
+  function prevSlideHandler() {
+    if (currentNavButton === 'prev') {
+      isDisableNavButtons = false;
+      if (currentPosition + columnWidth > 0) {
+        currentPosition = sliderContent.length * (- columnWidth - SLIDES_GAP);
+        clearAnimation();
+        setCurrentPosition();
+      }
+      setTimeout(() => {
+        setActiveSlide();
+      }, animationDuration / 3)
+      setAnimation();
+      sliderContentWrapper.removeEventListener("transitionend", nextSlideHandler);
+    }
+  }
+
+  function next() {
+    if (isDisableNavButtons) return;
+    isDisableNavButtons = true;
+    currentPosition = currentPosition - (columnWidth + SLIDES_GAP);
+    setCurrentPosition();
+    currentNavButton = 'next';
+    sliderContentWrapper.addEventListener("transitionend", nextSlideHandler);
+  }
+
+  function prev() {
+    if (isDisableNavButtons) return;
+    isDisableNavButtons = true;
+    currentPosition = currentPosition + (columnWidth + SLIDES_GAP);
+    setCurrentPosition();
+    currentNavButton = 'prev';
+    sliderContentWrapper.addEventListener("transitionend", prevSlideHandler);
+  }
+
+  nextButton.addEventListener("click", next);
+  prevButton.addEventListener("click", prev);
+
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowLeft') {
+      prev()
+    } else if (event.key === 'ArrowRight') {
+      next();
+    }
+  });
+
+  setTimeout(() => {
+    setActiveSlide();
+  }, animationDuration)
+}
+
+function mileSlider({
+  container,
+  animationDuration = 600,
+  animationType = 'linear',
+  height = 332,
+  slidesToShow = 3,
+  gap = 24,
+  activeSlideStyles = {}
+  } = {}) {
+  renderMileSlider({
+    container,
+    animationDuration,
+    animationType,
+    height,
+    slidesToShow,
+    gap,
+    activeSlideStyles
+  });
+
+  window.addEventListener("resize", () => {
+    document.querySelector(`#${container} .mile-slider__content div`).style.transition = 'none';
+    document.querySelectorAll(`#${container} .mile-slider__clone`).forEach(clone => clone.remove());
+    renderMileSlider({
+      container,
+      animationDuration,
+      animationType,
+      height,
+      slidesToShow,
+      gap,
+      activeSlideStyles
+    });
+  })
+}
+
+mileSlider({
+  container: 'slider1',
+  animationDuration: 600,
+  animationType: 'linear',
+  height: 332,
+  slidesToShow: 3,
+  gap: 24,
+  activeSlideStyles: {
+    backgroundColor: '#6554C0',
+    padding: '16px',
+  }
+});
